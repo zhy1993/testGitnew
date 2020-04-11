a5ed9d5b0f2fd7b5577cd2934c24d300dc92fca4
gitapace@.com <gitapace@.com>
2020-04-11 15:29:06 +0800

    idea修改 master add test class

---
+++ src/main/java/test.java
@@ -0,0 +1,11 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+/**
+ * @author Administrator
+ * @create 2020-04-11 15:27
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("master..........");
+    }
+}
