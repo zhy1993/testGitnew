f4e632c30d61d38771f7fed8ab624939a3b66e71
gitapace@.com <gitapace@.com>
2020-04-12 19:59:31 +0800

    idea修改  2 master add test class

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -6,6 +6,6 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
  */
 public class test {
     public static void main(String[] args) {
-        System.out.println("master..........");
+        System.out.println("master..........sdgfsdgfdgds");
     }
 }
